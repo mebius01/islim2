f6362f35e954f73ca30fa0825988ab40af1b58db
iv <consmebius@gmail.com>
2019-10-24 13:33:14 +0300

    add Catalog..

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,295 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <IsLim /> -->
+    <!-- <router-view/> -->
+    <div class="head">
+        <div class="welcome">
+          <p class="leftstr"><a href="">Welcome</a> | <a href="">Sin in</a> | <a href="">Register</a></p>
+          <p class="rightstr"> <a href="">How to buy</a> | <a href="">Customer</a> | <a href="">Payment</a></p>
+        </div>
+        <hr>
+        <div class="control">
+          <div class="logo">
+            ISLIM
+          </div>
+          <form role="search" action="">
+            <select name="Categories">
+              <option value="Categories">Categories</option>
+            </select>
+            <input type="text" name="search" v-model="searchName">
+            <button><i class="fas fa-search"></i> Search</button>
+          </form>
+          <div class="love">
+            <i class="far fa-user"></i>
+            <i class="far fa-heart"></i>
+            <i class="fas fa-shopping-bag"></i>
+          </div>
+        </div>
+        <hr>
+        <div class="bread_crumb">
+          <span>Clothing</span>
+          <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">All categories</a></li>
+            <li>Clothing</li>
+          </ul>
+        </div>
+    </div>
+    <Catalog />
+    <footer>
+      <div class="footer">
+        <div class="logo_foo">
+            <div class="logo">
+              ISLIM
+            </div>
+            <p>contact@leebros.us/p>
+            <p>+1 (00) 86 868 868 777</p>
+            <p>© 2019 ISLIM America Inc.</p>
+            <p>
+              <i class="fab fa-facebook-f"></i>
+              <i class="fab fa-instagram"></i>
+              <i class="fab fa-twitter"></i>
+            </p>
+        </div>
+        <div class="help_foo">
+          <div class="help">
+            HELP
+          </div>
+          <p>Orders & Returns</p>
+          <p>Search Terms</p>
+          <p>Advance Search</p>
+          <p>Affiliates</p>
+        </div>
+        <div class="acc_foo">
+          <div class="account">
+            MY ACCOUNT
+          </div>
+          <p>About Us</p>
+          <p>Delivery Information</p>
+          <p>Privacy & Policy</p>
+          <p>Terms & Conditions</p>
+        </div>
+        <div class="contact_foo">
+          <div class="contact">
+            CONTACT
+          </div>
+          <form action="">
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Email Address">
+            <button type="submit">OK</button>
+          </form>
+        </div>
+        <div class="pay">
+          <p>
+            <i class="fab fa-cc-visa"></i>
+            <i class="fab fa-cc-mastercard"></i>
+            <i class="fab fa-cc-stripe"></i>
+            <i class="fab fa-cc-paypal"></i>
+          </p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Catalog from './components/Catalog.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Catalog
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap&subset=cyrillic,cyrillic-ext,latin-ext,vietnamese');
+html, body {
+    width: 100%;
+    height: 100%;
+    font-family: Montserrat;
+}
+:root{
+  --global-width:1130px;
+}
+hr{
+  align: center;
+  width: 100%;
+  size: 1;
+  color: #B0BEC5;
+}
+#app{
+  /* width: 1130px;
+  margin-left: auto;
+  margin-right: auto; */
+  min-height: 100%;
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto auto auto;
+  grid-template-areas:
+    "head"
+    "bread_crumb"
+    "catalog"
+    /* "aside article" */
+    /* "paginator paginator" */
+    "footer"
+
+}
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  /* color: #2d3e4e;
+} */
+.head{
+  grid-area: head;
+}
+.welcome{
+  width: var(--global-width);
+  margin-left: auto;
+  margin-right: auto;
+  color: #B1B8BB;
+}
+.welcome a{
+  color: #B1B8BB;
+  text-decoration: none;
+  font-size: 14px;
+  line-height: 17px;
+}
+.leftstr, .rightstr {
+  float: left; /* Обтекание справа */
+  width: 50%; /* Ширина текстового блока */
+}
+.rightstr {
+  text-align: right; /* Выравнивание по правому краю */
+}
+.control{
+  width: var(--global-width);
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: 15% 1fr 15%;
+  margin-top: 38px;
+  margin-bottom: 38px;
+}
+.control .logo{
+  width: 101px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 36px;
+  line-height: 44px;
+  color: #424140;
+}
+.control .love{
+  justify-self: end;
+  height: 38px;
+}
+.control .love i{
+  margin-left: 21px;
+  margin-top: 15px;
+  font-size: 21px;
+  text-align: left;
+  color: #B1B8BB;
+}
+.control input{
+  border: 1px solid #B1B8BB;
+  width: 570px;
+  height: 35px;
+}
+.control select, .control button{
+  border: 1px solid #B1B8BB;
+  height: 38px;
+  box-sizing: border-box;
+  color: #B1B8BB;
+}
+.control select{
+  border-radius: 4px 0px 0px 4px;
+  background-color: #ffffff;
+  width: 113px;
+}
+.control button{
+  border-radius: 0px 4px 4px 0px;
+  width: 97px;
+}
+.bread_crumb{
+  width: var(--global-width);
+  margin-left: auto;
+  margin-right: auto;
+  grid-area: bread_crumb;
+}
+.bread_crumb span{
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 33px;
+  color: #000000;
+}
+ul.breadcrumb {
+  margin-top: 6px;
+  padding: 0;
+  list-style: none;
+}
+ul.breadcrumb li {
+  display: inline;
+  color: #828C90;
+}
+ul.breadcrumb a{
+  text-decoration: none;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 29px;
+  color: #828C90;
+}
+ul.breadcrumb li+li:before {
+  padding: 8px;
+  color: black;
+  content: "\003e";
+  color: #828C90;
+}
+aside{
+  width: 250px;
+  justify-self: end;
+  grid-area: aside;
+}
+
+article{
+  width: 880px;
+  grid-area: article;
+}
+footer {
+  /* height: 258px; */
+  background-color: #E5E5E5;
+  grid-area: footer;
+}
+.footer{
+  width: var(--global-width);
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 18px;
+  display: grid;
+  grid-template-columns: repeat(5 1fr);
+  grid-template-rows: 80% 20%;
+  grid-template-areas:
+    "l_foo h_foo a_foo c_foo"
+    "pay pay pay pay"
+}
+.logo_foo{
+  grid-area: l_foo;
+}
+.help_foo{
+  grid-area: h_foo;
+}
+.acc_foo{
+  grid-area: a_foo;
+}
+.contact_foo{
+  grid-area: c_foo;
+}
+.pay{
+  grid-area: pay;
+  justify-self: end;
 }
 </style>
---
+++ src/components/Catalog.vue
@@ -0,0 +1,428 @@
+<template>
+  <div class="catalog">
+    <aside>
+      <div class="filters">
+        <div class="price">
+            <span class="name_filter">Price</span>
+            <form action="" name="price">
+            <input class="min" type="text" placeholder="min">
+            <input class="max" type="text" placeholder="max">
+            <input type="range" name="points" min="0" max="10">
+            </form>
+        </div>
+        <div class="gender">
+            <span class="name_filter">Gender</span>
+            <br>
+            <label class="date_filter">Man
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+            </label>
+            <br>
+            <label class="date_filter">Women <span class="count">(118)</span>
+            <input type="checkbox">
+            <span class="checkmark"></span>
+            </label>
+        </div>
+        <div class="category">
+            <span class="name_filter">Product Category</span>
+            <div v-show="visible_category">
+              <div v-for="c in productCategories.slice(0,4)" v-bind:key="c.id">
+                <label class="date_filter">{{c}} <span class="count">(COUNTER)</span>
+                  <input type="checkbox" :id="c.id" :value="c" v-model="tagList">
+                  <span class="checkmark"></span>
+                </label>
+              </div>
+            </div>
+            <div v-show="!visible_category">
+              <div v-for="c in productCategories" v-bind:key="c.id">
+                <label class="date_filter">{{c}} <span class="count">(COUNTER)</span>
+                  <input type="checkbox" :id="c.id" :value="c" v-model="tagList">
+                  <span class="checkmark"></span>
+                </label>
+                </div>
+              </div>
+              <a class="view_more" @click="visible_category=!visible_category">{{visible_category?"View More":"Hide"}}</a>
+        </div>
+        <!-- <div class="color">
+            <span class="name_filter">Colour</span>
+            <div v-show="visible_color">
+              <div v-for="s in info.slice(0,4)" v-bind:key="s.id">
+                <label class="date_filter">{{s.color}}
+                  <input type="checkbox" :id="s.id" :value="s.color" v-model="tagList">
+                  <span class="checkmark" v-bind:style="{background: s.color}"></span>
+                </label>
+                <br>
+              </div>
+            </div>
+            <div v-show="!visible_color">
+              <div v-for="s in productColor" v-bind:key="s.id">
+                <label class="date_filter">{{s}}
+                  <input type="checkbox" :id="s.id" :value="s" v-model="tagList">
+                  <span class="checkmark" v-bind:style="{background: s}"></span>
+                </label>
+                <br>
+              </div>
+            </div>
+        <a class="view_more" @click="visible_color=!visible_color">{{visible?"View More":"Hide"}}</a>
+        </div> -->
+      <!-- <button @click="AddTagList">add Tag</button> -->
+      </div>
+    </aside>
+    <article>
+      <div>
+        <div class="tag_section">
+          <div class="tag_list" v-for="i in tagList" v-bind:key="i.id">
+            <button class="tag">{{i}} <span style="color:#E44747">x</span></button>
+          </div>
+        <a href="">Clear all</a>
+        <select name="Sort By">
+          <option value="Categories">Sort By +</option>
+          <option value="Categories">Sort By -</option>
+        </select>
+      </div>
+      <div class="islim">
+        <div v-for="item in info" v-bind:key="item.id">
+          <div class="card">
+            <div class="sale">SALE</div>
+            <a class="heart" href=""><i class="far fa-heart"></i></a>
+            <img src="../assets/250.png" :alt="item.name" :title="item.name" width="250" height="250">
+            <div class="name">
+                {{item.id}}-{{item.name}}
+              </div>
+            <div class="foot_card">
+              <div class="descript">
+                {{item.description}}
+              </div>
+            </div>
+            <div class="price">
+                $ {{item.price}}
+              </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </article>
+    <div class="paginator">
+      <a href="#">&laquo;</a>
+      <a href="#">1</a>
+      <a class="active" href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#">6</a>
+      <a href="#">&raquo;</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Catalog',
+  data () {
+    return {
+      info: null,
+      tagList: [],
+      visible_category: true,
+      visible_color: true,
+      colors: [],
+      category: []
+    }
+  },
+  // methods: {
+  //   AddTagList () {
+  //     console.log('AddTagList', this.tagList)
+  //   }
+  // },
+  mounted () {
+    axios
+      .get('http://localhost:3000/products')
+      .then(response => (this.info = response.data))
+  },
+  computed: {
+    productCategories () {
+      return [...new Set(this.info.map(({ category }) => category))]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.catalog{
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto auto;
+  grid-area: catalog;
+  grid-template-areas:
+    "aside article"
+    "paginator paginator"
+}
+.view_more{
+  cursor: pointer;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 26px;
+  text-decoration-line: underline;
+  color: #9AA2A9;
+}
+.max, .min{
+  width: 100px;
+  height: 33px;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 29px;
+  color: #000000;
+  margin-bottom: 20px;
+}
+.max{
+  float: right;
+}
+.filters .date_filter {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  cursor: pointer;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 17px;
+  color: #000000;
+  text-transform: capitalize;
+}
+
+/* Hide the browser's default checkbox */
+.filters .date_filter input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* квадраты */
+.filters .checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 19px;
+  width: 19px;
+  border: 1px solid #C4C4C4;
+  background-color: #FFFFFF;
+}
+
+/* Навести мышой на квадрат background color
+.filters .date_filter:hover input ~ .checkmark {
+  background-color: #C4C4C4;
+} */
+
+/* background желтый при checked */
+.filters .date_filter input:checked ~ .checkmark {
+  background-color: #FFCA28;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.filters .date_filter input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style для галки */
+.filters .date_filter .checkmark:after {
+  left: 6px;
+  top: 2px;
+  width: 5px;
+  height: 10px;
+  border: 1px solid #000000;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+.price .name_filter, .gender .name_filter,
+.category .name_filter, .size .name_filter,
+.brend .name_filter, .style .name_filter,
+.season .name_filter, .material .name_filter,
+.color .name_filter{
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 26px;
+  color: #000000;
+}
+.price, .gender,
+.category, .size,
+.brend, .style,
+.season, .material,
+.color{
+  margin-bottom: 20px;
+}
+/* .price label, .gender label,
+.category label, .size label,
+.brend label, .style label,
+.season label, .material label,
+.color label{
+  margin-top: 10px;
+} */
+.price .count, .gender .count,
+.category .count, .size .count,
+.brend .count, .style .count,
+.season .count, .material .count,
+.color .count{
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 26px;
+  text-transform: lowercase;
+  color: #C4C4C4;
+}
+input[type=range]{
+  width: 100%;
+}
+
+.islim{
+  margin-left: 40px;
+  display: grid;
+  gap: 40px;
+  grid-template-columns: repeat(auto-fit, minmax(250px, auto));
+  grid-template-rows: repeat(auto-fit, minmax(350, auto));
+}
+.tag_list {
+  display: inline;
+}
+.tag_section{
+  margin-top: 0px;
+  margin-bottom: 22px;
+  margin-left: 40px;
+}
+.tag_section button{
+  margin-right: 9px;
+  border: 1px solid #B0BEC5;
+  box-sizing: border-box;
+  border-radius: 4px;
+  background-color: #FFFFFF;
+  font-weight: 300;
+  font-size: 12px;
+  line-height: 22px;
+  color: #000000;
+}
+.tag_section a{
+  font-weight: 300;
+  font-size: 12px;
+  line-height: 22px;
+  text-decoration: none;
+  text-transform: capitalize;
+  color: #000000;
+}
+.tag_section select{
+  float:right;
+  width: 150px;
+  height: 28px;
+  border: 1px solid #B0BEC5;
+  box-sizing: border-box;
+  border-radius: 4px;
+  background-color: #FFFFFF;
+  font-weight: 300;
+  font-size: 12px;
+  line-height: 22px;
+  text-transform: capitalize;
+  color: #000000;
+}
+.card {
+  position: relative;
+  border-radius: 4px;
+  width: 250px;
+  height: 350px;
+  display: grid;
+  background: #FFFFFF;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+  color:  var(--global_text_color);
+  text-align:left;
+  position: relative;
+}
+.card img{
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+}
+.heart{
+  position: absolute;
+  top: 13px;
+  right: 13px;
+  color: #A3A3A3;
+}
+.sale{
+  position: absolute;
+  top:12px;
+  left:9px;
+  transform: rotate(-90deg);
+  background-color: #FFCA28;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 12px;
+  padding-right: 9px;
+  padding-left: 2px;
+}
+.foot_card, .price, .name{
+  /* margin-top: 7px;
+  margin-bottom: 7px; */
+  margin-left: 12px;
+  margin-right: 14px;
+}
+.name{
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+  text-transform: uppercase;
+  color: #000000;
+}
+.descript{
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 17px;
+  color: #000000;
+}
+.price{
+  align-self: end;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 22px;
+  /* identical to box height */
+  color: #000000;
+  margin-bottom: 4px;
+}
+.paginator {
+    justify-self: center;
+    /* background-color: bisque; */
+    grid-area: paginator;
+    display: inline-block;
+    margin-top: 40px;
+    margin-bottom: 40px;
+}
+.paginator a {
+    border: 1px solid #EAF1F4;
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+    margin-left: 10px;
+    border-radius: 5px;
+    color: #B0BEC5;
+}
+.paginator a.active{
+    background: #4A5E69;
+}
+.paginator a:hover{
+    background: #4A5E69;
+}
+</style>
